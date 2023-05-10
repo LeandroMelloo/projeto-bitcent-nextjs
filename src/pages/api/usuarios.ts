276bf47d154944a6a77e0ad45e0d4a6b16e3a51a
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-05-10 16:25:20 -0300

    Criando um componete de exemplo

--- src/pages/api/usuarios.ts
+++ src/pages/api/usuarios.ts
@@ -4,6 +4,7 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    //http://localhost:3000/api/usuarios
     res.status(200).json([
         { name: 'Leandro', email: 'leandro@gmail.com' },
         { name: 'Luciana', email: 'luciana@gmail.com' },
